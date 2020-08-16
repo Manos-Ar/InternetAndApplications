f912e41be56fd1a610c259b6a2f284d968b01106
Manos-Ar <arapidismanolis@gmail.com>
2020-08-16 15:29:41 +0300

    minor changes

--- frontend/src/service/Season.js
+++ frontend/src/service/Season.js
@@ -71,9 +71,6 @@ class Season extends Component{
     const Size = this.state.Size;
 
     // console.log('Submitting...', Year, Position, Criteria, Size);
-
-    // fetch('/energy/api/'+Dataset+'/'+AreaName+'/'+Resolution+'/'+SearchPeriod+'/'+Date,{
-    // fetch("http://localhost:8080/NBA/"+Year+'/'+Position+'/'+Criteria+'/'+Size,{
 		if(Year==null || Position==null || Criteria==null || Size==null) {
 				this.setState({
 					URLError: true
@@ -88,7 +85,6 @@ class Season extends Component{
 		else{
     fetch(url+Year+'/'+Position+'/'+Criteria+'/'+Size,{
       method: 'GET',
-      // headers: {'X-OBSERVATORY-AUTH': this.context.token}
     })
     /* Returns a promise containing the response */
       .then((response) => {
@@ -135,108 +131,103 @@ class Season extends Component{
 		const isSubmitted = this.state.isSubmitted;
 
 
-// 			if (URLError){
-// 				return (<div class="alert"><h1><strong>Error: </strong>Fill all fields</h1> Refresh the page.</div>);
-// }
-// 			else {
 				return(
-					<div className="center">
-    <div className="container-lg" >
-    <form className="was-validated" onSubmit={this.handleSubmit}>
+		<div className="center">
+    	<div className="container-lg" >
+    		<form className="was-validated" onSubmit={this.handleSubmit}>
 
-    <div className=''>
-    <h1>NBA Players Statistics</h1>
-    </div>
+    			<div className=''>
+  					<h1>NBA Players Statistics</h1>
+    			</div>
 
-    <div id="Position">
-      <div className="form-row">
-        <div className="form-group col-md-10">
-          <label htmlFor="validationDefault05">Position</label>
-          <select required className="custom-select" name="Position" value={this.state.Position} onChange={this.handleChange}>
-            <option disabled selected value> -- Select Position -- </option>
-            {this.createSelect(optionsPositions)}
-          </select>
-					<div className="invalid-feedback">Please select a value</div>
-        </div>
-      </div>
-    </div>
+					<div id="Position">
+					  <div className="form-row">
+					    <div className="form-group col-md-10">
+					      <label htmlFor="validationDefault05">Position</label>
+					      <select required className="custom-select" name="Position" value={this.state.Position} onChange={this.handleChange}>
+					        <option disabled selected value> -- Select Position -- </option>
+					        {this.createSelect(optionsPositions)}
+					      </select>
+								<div className="invalid-feedback">Please select a value</div>
+					    </div>
+					  </div>
+					</div>
 
-    <div id="Criteria">
-      <div className="form-row">
-        <div className="form-group col-md-10">
-          <label htmlFor="validationDefault03">Criteria</label>
-          <select required className="custom-select" name="Criteria" value={this.state.Criteria} onChange={this.handleChange}>
-            <option disabled selected value> -- Select Criteria -- </option>
-            {this.createSelect(optionsCriteria)}
-          </select>
-					<div className="invalid-feedback">Please select a value</div>
-        </div>
-      </div>
-    </div>
+					<div id="Criteria">
+					  <div className="form-row">
+					    <div className="form-group col-md-10">
+					      <label htmlFor="validationDefault03">Criteria</label>
+					      <select required className="custom-select" name="Criteria" value={this.state.Criteria} onChange={this.handleChange}>
+					        <option disabled selected value> -- Select Criteria -- </option>
+					        {this.createSelect(optionsCriteria)}
+					      </select>
+								<div className="invalid-feedback">Please select a value</div>
+					    </div>
+					  </div>
+					</div>
 
-		<div className="form-row">
-			<div className="form-group col-md-10">
-				<label htmlFor="validationDefault03">Year</label>
-				<input required className="form-control" name="Year" id="DateNone" type="text" placeholder="YYYY" value={this.state.Year} onChange={this.handleChange}
-					pattern="^[0-9]{4}"/>
-				<div className="invalid-feedback">Please input a value</div>
+					<div className="form-row">
+						<div className="form-group col-md-10">
+							<label htmlFor="validationDefault03">Year</label>
+							<input required className="form-control" name="Year" id="DateNone" type="text" placeholder="YYYY" value={this.state.Year} onChange={this.handleChange}
+								pattern="^[0-9]{4}"/>
+							<div className="invalid-feedback">Please input a value</div>
+						</div>
+					</div>
+
+					<div className="form-row">
+						<div className="form-group col-md-10">
+							<label htmlFor="validationDefault03">Size</label>
+							<input required className="form-control" name="Size" id="DateNone" type="text" placeholder="XXX" value={this.state.Size} onChange={this.handleChange}
+								pattern="^[0-9]{3}|[0-9]{2}|[0-9]{1}"/>
+							<div className="invalid-feedback">Please input a value</div>
+						</div>
+					</div>
+
+					<div className="left">
+			    	<button class="button button2" type="submit">Search</button>
+					</div>
+    		</form>
+				<div>
+					{(URLError)?<p><strong>Error: </strong>Fill all fields</p>:<div/>}
+					{(InvalidYear)?<p>Invalid Year Range. Choose between 1950-2017</p>:<div/>}
+					{(isSubmitted)
+						?(
+					<div id="span3" className="table-responsive -sm">
+						<table className="table table-hover">
+							<thead class="thead-dark">
+								<tr>
+									<th scope="col">Year</th>
+									<th scope="col">Player</th>
+									<th scope="col">Positon</th>
+									<th scope="col">Team</th>
+									<th scope="col">Number of Games</th>
+									<th scope="col">3Pointer%</th>
+									<th scope="col">Assists</th>
+									<th scope="col">Total Points</th>
+								</tr>
+							</thead>
+							<tbody>
+							{this.state.Results.map(item => (
+								<tr key={shortid.generate()}>
+									<td>{item.year}</td>
+									<td>{item.player}</td>
+									<td>{item.pos}</td>
+									<td>{item.tm}</td>
+									<td>{item.g}</td>
+									<td>{item.p3_Per}</td>
+									<td>{item.ast}</td>
+									<td>{item.pts}</td>
+								</tr>
+							)
+						)}
+							</tbody>
+						</table>
+					</div>
+
+			  )
+				:(<div></div>)}
 			</div>
-		</div>
-
-		<div className="form-row">
-			<div className="form-group col-md-10">
-				<label htmlFor="validationDefault03">Size</label>
-				<input required className="form-control" name="Size" id="DateNone" type="text" placeholder="XXX" value={this.state.Size} onChange={this.handleChange}
-					pattern="^[0-9]{3}|[0-9]{2}|[0-9]{1}"/>
-				<div className="invalid-feedback">Please input a value</div>
-			</div>
-		</div>
-
-		<div className="left">
-    <button class="button button2" type="submit">Search</button>
-		</div>
-    </form>
-		<div>
-		{(URLError)?<p><strong>Error: </strong>Fill all fields</p>:<div/>}
-		{(InvalidYear)?<p>Invalid Year Range.
-			Choose between 1950-2017</p>:<div/>}
-		{(isSubmitted)
-			?(
-		<div id="span3" className="table-responsive -sm">
-			<table className="table table-hover">
-				<thead class="thead-dark">
-					<tr>
-						<th scope="col">Year</th>
-						<th scope="col">Player</th>
-						<th scope="col">Positon</th>
-						<th scope="col">Team</th>
-						<th scope="col">Number of Games</th>
-						<th scope="col">3Pointer%</th>
-						<th scope="col">Assists</th>
-						<th scope="col">Total Points</th>
-					</tr>
-				</thead>
-				<tbody>
-				{this.state.Results.map(item => (
-					<tr key={shortid.generate()}>
-						<td>{item.year}</td>
-						<td>{item.player}</td>
-						<td>{item.pos}</td>
-						<td>{item.tm}</td>
-						<td>{item.g}</td>
-						<td>{item.p3_Per}</td>
-						<td>{item.ast}</td>
-						<td>{item.pts}</td>
-					</tr>
-				)
-			)}
-				</tbody>
-			</table>
-		</div>
-
-  )
-	:(<div></div>)}
-</div>
 
 	</div>
 	</div>
